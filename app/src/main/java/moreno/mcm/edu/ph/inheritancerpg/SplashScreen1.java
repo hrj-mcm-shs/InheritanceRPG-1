1feec24448c772e0bd06c25ead5a9488fc17a65a
rendelljames33 <ninavalera019@gmail.com>
2020-03-13 12:27:49 +0800

    init

--- app/src/main/java/moreno/mcm/edu/ph/inheritancerpg/SplashScreen1.java
+++ app/src/main/java/moreno/mcm/edu/ph/inheritancerpg/SplashScreen1.java
@@ -9,6 +9,8 @@ import android.transition.Fade;
 
 public class SplashScreen1 extends AppCompatActivity {
     Handler handler;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
